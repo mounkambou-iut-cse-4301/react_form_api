fc0a184d5ca803f870a3b4a1d113a09a26ca0f39
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2021-08-28 15:46:26 +0100

    Update table

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -34,7 +34,7 @@ class Student extends Component {
                             <td>{item.email}</td>
                             <td>{item.phone}</td>
                             <td>
-                                <Link to={`edit-student/{item.id}`} className="btn btn-success btn-sm">Edit</Link>
+                                <Link to={`edit-student/${item.id}`} className="btn btn-success btn-sm">Edit</Link>
                             </td>
                             <td>
                                 <button type="button" className="btn btn-danger btn-sm">Delete</button>
