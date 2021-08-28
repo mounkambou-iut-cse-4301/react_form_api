6da0c688cd09f751a8be6b31b432b8b214f0cd2d
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2021-08-28 12:07:47 +0100

    get data form database

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,8 +1,48 @@
+import axios from 'axios';
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Student extends Component {
+    state = {
+        students: [],
+        loading: true,
+    }
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:8000/api/students');
+        if (res.data.status === 200) {
+
+            this.setState({
+                students: res.data.students,
+                loading: false,
+            });
+        }
+    }
+
     render() {
+
+        var student_HTMLTABLE = "";
+        if (this.state.loading) {
+            student_HTMLTABLE = <tr><td colSpan="7"><h2>Loading....</h2></td></tr>
+        } else {
+            student_HTMLTABLE =
+                this.state.students.map((item) => {
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.course}</td>
+                            <td>{item.email}</td>
+                            <td>{item.phone}</td>
+                            <td>
+                                <Link to={`edit-student/{item.id}`} className="btn btn-success btn-sm">Edit</Link>
+                            </td>
+                            <td>
+                                <button type="button" className="btn btn-danger btn-sm">Delete</button>
+                            </td>
+                        </tr>
+                    )
+                })
+        }
         return (
             <div className="container">
                 <div className="row">
@@ -14,6 +54,22 @@ class Student extends Component {
                                 </h4>
                             </div>
                             <div className="card-body">
+                                <table className="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Course</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>Edit</th>
+                                            <th>Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {student_HTMLTABLE}
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
